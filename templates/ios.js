ee3bd57c75d12c44e6ce6b5200c840cc521f8003
Christopher J. Brody <chris@brody.consulting>
2019-02-26 17:28:55 -0500

    Remove methodQueue method on iOS

--- templates/ios.js
+++ templates/ios.js
@@ -44,11 +44,6 @@ end
 
 RCT_EXPORT_MODULE()
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
-
 RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
 {
     // TODO: Implement some real useful functionality
