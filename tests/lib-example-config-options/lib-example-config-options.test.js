e92044fc106986ddf6b37df6577ceabf3e69874d
Christopher J. Brody <chris@brody.consulting>
2019-07-23 14:13:19 -0400

    rename & update tests/lib-example-options
    rename the test to:
    
    * tests/lib-example-config-options/lib-example-config-options.test.js
    
    with updated title, config options added, and __snapshots__ updated
    (regenerated)

--- tests/lib-example-config-options/lib-example-config-options.test.js
+++ tests/lib-example-config-options/lib-example-config-options.test.js
@@ -2,13 +2,17 @@ const lib = require('../../lib/lib.js');
 
 const ioMocks = require('../helpers/io-mocks.js');
 
-test('create alice-bobbi module with defaults', () => {
+test('create alice-bobbi module with example, with config options', () => {
   const mysnap = [];
 
   const mocks = ioMocks(mysnap);
 
   const options = {
     name: 'alice-bobbi',
+    githubAccount: 'alicebits',
+    authorName: 'Alice',
+    authorEmail: 'contact@alice.me',
+    license: 'ISC',
     generateExample: true,
     exampleName: 'test-demo',
     exampleReactNativeVersion: 'react-native@0.60',
