adc283a55eb9bb403b576178c56a5fcdb86a76cd
Christopher J. Brody <chris@brody.consulting>
2019-07-23 11:29:51 -0400

    add tests/lib-view-ios-config-options

---
+++ tests/lib-view-ios-config-options/lib-view-ios-config-options.test.js
@@ -0,0 +1,23 @@
+const lib = require('../../lib/lib.js');
+
+const ioMocks = require('../helpers/io-mocks.js');
+
+test('create alice-bobbi view module with config options for iOS only', () => {
+  const mysnap = [];
+
+  const mocks = ioMocks(mysnap);
+
+  const options = {
+    platforms: ['ios'],
+    name: 'alice-bobbi',
+    githubAccount: 'alicebits',
+    authorName: 'Alice',
+    authorEmail: 'contact@alice.me',
+    license: 'ISC',
+    view: true,
+    fs: mocks.fs,
+  };
+
+  return lib(options)
+    .then(() => { expect(mysnap).toMatchSnapshot(); });
+});
