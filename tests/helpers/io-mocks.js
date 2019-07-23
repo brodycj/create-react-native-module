587a34a914000c3f76787e4c6ceb2457ac08e645
Christopher J. Brody <chris@brody.consulting>
2019-07-23 02:17:53 -0400

    Add tests with generateExample: true setting

--- tests/helpers/io-mocks.js
+++ tests/helpers/io-mocks.js
@@ -17,4 +17,21 @@ ${content}
       return Promise.resolve();
     },
   },
+  execa: {
+    commandSync: (command, options) => {
+      mysnap.push(
+        `* execa.commandSync command: ${command} options: ${JSON.stringify(options)}\n`);
+    },
+  },
+  jsonfile: {
+    readFileSync: (jsonPath) => {
+      mysnap.push(
+        `* jsonfile.readFileSync jsonPath: ${jsonPath}\n`);
+      return { name: 'bogus', scripts: [] };
+    },
+    writeFileSync: (path, json, options) => {
+      mysnap.push(
+        `* jsonfile.writeFileSync path: ${path} json ${JSON.stringify(json)} options ${JSON.stringify(options)}\n`);
+    },
+  },
 });
---
+++ tests/lib-example-options/lib-example-options.test.js
@@ -0,0 +1,21 @@
+const lib = require('../../lib/lib.js');
+
+const ioMocks = require('../helpers/io-mocks.js');
+
+test('create alice-bobbi module with defaults', () => {
+  const mysnap = [];
+
+  const mocks = ioMocks(mysnap);
+
+  const options = {
+    name: 'alice-bobbi',
+    generateExample: true,
+    exampleName: 'test-demo',
+    exampleReactNativeVersion: 'react-native@0.60',
+    fs: mocks.fs,
+    execa: mocks.execa,
+    jsonfile: mocks.jsonfile,
+  };
+
+  return lib(options).then(() => { expect(mysnap).toMatchSnapshot(); });
+});
