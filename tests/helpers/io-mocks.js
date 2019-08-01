83179393a4a44009073c86da9425f980db4fb719
Christopher J. Brody <chris@brody.consulting>
2019-08-01 10:21:04 -0400

    jsonfile test updates & fixes
    * more realistic example package.json object, which fixes capture of
      the new scripts entry that is added to the example package.json object
    
    * push more formal entries to the I/O snapshot
    
    * move mock jsonfile definition directly below mock fs
      definition, to make them easier to combine in the future

--- tests/helpers/io-mocks.js
+++ tests/helpers/io-mocks.js
@@ -17,21 +17,32 @@ ${content}
       return Promise.resolve();
     },
   },
+  jsonfile: { // FUTURE TBD may be combined with fs object above
+    readFileSync: (jsonFilePath) => {
+      mysnap.push({
+        call: 'jsonfile.readFileSync',
+        jsonFilePath,
+      });
+      return {
+        name: 'example',
+        scripts: {
+          test: 'echo "not implemented" && exit 1'
+        }
+      };
+    },
+    writeFileSync: (path, json, options) => {
+      mysnap.push({
+        call: 'jsonfile.writeFileSync',
+        filePath: path,
+        json,
+        options,
+      });
+    },
+  },
   execa: {
     commandSync: (command, options) => {
       mysnap.push(
         `* execa.commandSync command: ${command} options: ${JSON.stringify(options)}\n`);
-    },
-  },
-  jsonfile: {
-    readFileSync: (jsonPath) => {
-      mysnap.push(
-        `* jsonfile.readFileSync jsonPath: ${jsonPath}\n`);
-      return { name: 'bogus', scripts: [] };
-    },
-    writeFileSync: (path, json, options) => {
-      mysnap.push(
-        `* jsonfile.writeFileSync path: ${path} json ${JSON.stringify(json)} options ${JSON.stringify(options)}\n`);
     },
   },
 });
