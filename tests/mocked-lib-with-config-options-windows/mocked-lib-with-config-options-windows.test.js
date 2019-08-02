a304ead32defee90e96976a0ed3c54f947d9ed87
Christopher J. Brody <chris@brody.consulting>
2019-08-01 21:11:37 -0400

    fully mocked library test for Windows
    (using real Jest mocks)
    
    with some config options
    
    based on rename of:
    
    * tests/lib-windows-config-options/lib-windows-config-options.test.js
    
    Rationale: it is desired to have fully mocked coverage of the
    library behavior on Windows, to avoid unexpected breaking changes,
    while additional testing is not really desired on that
    deprecated platform.

--- tests/mocked-lib-with-config-options-windows/mocked-lib-with-config-options-windows.test.js
+++ tests/mocked-lib-with-config-options-windows/mocked-lib-with-config-options-windows.test.js
@@ -4,12 +4,29 @@ jest.mock('uuid', () => ({
   v1: () => 'E22606E0-B47F-11E9-A3F0-07F70A25DAFB'
 }));
 
+/* XXX TBD GONE:
 const ioMocks = require('../helpers/io-mocks.js');
+// */
 
-test('create alice-bobbi module with config options for Windows only', () => {
+const mysnap = [];
+const mockpushit = x => mysnap.push(x);
+jest.mock('fs-extra', () => ({
+  outputFile: (outputFileName, theContent) => {
+    mockpushit({ outputFileName, theContent });
+    return Promise.resolve();
+  },
+  ensureDir: (dir) => {
+    mockpushit({ ensureDir: dir });
+    return Promise.resolve();
+  },
+}));
+
+test('create alice-bobbi module using mocked lib with config options on Windows', () => {
+  /* XXX TBD GONE:
   const mysnap = [];
 
   const mocks = ioMocks(mysnap);
+  // */
 
   const options = {
     platforms: ['windows'],
@@ -19,7 +36,6 @@ test('create alice-bobbi module with config options for Windows only', () => {
     authorEmail: 'contact@alice.me',
     license: 'ISC',
     namespace: 'Carol',
-    fs: mocks.fs,
   };
 
   return lib(options)
