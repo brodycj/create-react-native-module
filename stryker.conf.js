81100f82470007bcc68ff6e6a037111c2aed737c
Christopher J. Brody <chris@brody.consulting>
2019-08-30 03:43:52 -0400

    Introduce Stryker mutator
    using the following command: stryker init
    
    according to: https://stryker-mutator.io/stryker/quickstart
    
    with HTML reporter included
    
    and some manual updates:
    
    * new run-stryker task script in package.json
    * add space after `function` in stryker.conf.js
      (to make eslint with rules from "Standard JS" happy)
    * add .eslintignore to ignore generated reports
    * .gitignore updates to ignore generated artifacts

---
+++ stryker.conf.js
@@ -0,0 +1,10 @@
+module.exports = function (config) {
+  config.set({
+    mutator: "javascript",
+    packageManager: "yarn",
+    reporters: ["html", "clear-text", "progress"],
+    testRunner: "jest",
+    transpilers: [],
+    coverageAnalysis: "off"
+  });
+};
