0594780fa64e339cf3ae09b289302b68a70d6b22
Christopher J. Brody <chris@brody.consulting>
2019-09-04 18:18:35 -0400

    test with custom prefix for Android & iOS
    covers extra breaking mutation 2a from PR #84:
    
    diff --git a/lib/normalized-options.js b/lib/normalized-options.js
    index 9c49d88..773981d 100644
    --- a/lib/normalized-options.js
    +++ b/lib/normalized-options.js
    @@ -8,7 +8,7 @@ const DEFAULT_MODULE_PREFIX = 'react-native';
     module.exports = (options) => {
       const name = options.name;
    
    -  const prefix = options.prefix || '';
    +  const prefix = '';
    
       const modulePrefix = options.modulePrefix || DEFAULT_MODULE_PREFIX;

--- tests/with-injection/create/with-options/platforms-array/platforms-array.test.js
+++ tests/with-injection/create/with-options/platforms-array/platforms-array.test.js
@@ -10,6 +10,7 @@ test('create alice-bobbi module with config options for android,ios comma separa
   const options = {
     platforms: ['android', 'ios'],
     name: 'alice-bobbi',
+    prefix: 'ABC',
     githubAccount: 'alicebits',
     authorName: 'Alice',
     authorEmail: 'contact@alice.me',
