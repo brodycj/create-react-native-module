b25da1cdca079e8687e01bdee5baad41b90110e9
Christopher J. Brody <chris@brody.consulting>
2019-09-20 13:41:27 -0400

    consistent iOS method spacing in generated code
    * continue using tabs in case of `useCocoapods: true`
    * with 4 spaces in case of no truthy `useCocoapods` option
    
    with a comment removed in case of `useCocoapods: true`

--- templates/ios.js
+++ templates/ios.js
@@ -48,18 +48,22 @@ ${useCocoapods ? `#import <AFNetworking/AFNetworking.h>
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-	${useCocoapods
-    ? `AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+` +
+    // sample method body with tabs for truthy useCocoapods *only*:
+    (useCocoapods
+      ? `{
+	AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
 	manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/plain"];
 	[manager GET:@"https://httpstat.us/200" parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
 			callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@ resp: %@", numberArgument, stringArgument, responseObject]]);
 	} failure:^(NSURLSessionTask *operation, NSError *error) {
 			callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@ err: %@", numberArgument, stringArgument, error]]);
-	}];`
-    : `callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);`}
-}
+	}];
+}`
+      : `{
+    // TODO: Implement some actually useful functionality
+    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+}`) + `
 
 @end
 `,
