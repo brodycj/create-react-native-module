0e982e4fd1bb99c01364947e9255931b4f2e35e4
Christopher J. Brody <chris@brody.consulting>
2019-10-17 16:12:38 -0400

    update stryker.conf.js to include templates

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,6 +1,7 @@
 module.exports = config => {
   config.set({
     mutator: 'javascript',
+    mutate: ['lib/**/*.js', 'templates/**/*.js'],
     packageManager: 'yarn',
     reporters: ['html', 'clear-text', 'progress'],
     testRunner: 'jest',
