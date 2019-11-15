cc22885a0e3b1889ca96921c3969215780ecf9e9
Chris Brody <chris@brody.consulting>
2019-11-14 23:34:49 -0500

    remove trailing tabs from generated podspec file (#146)
    This is a quick fix update which leaves an extra comment line
    in one of the generated podspec files.

--- templates/ios.js
+++ templates/ios.js
@@ -22,7 +22,7 @@ Pod::Spec.new do |s|
   s.requires_arc = true
 
   s.dependency "React"
-	${useAppleNetworking ? `s.dependency 'AFNetworking', '~> 3.0'` : ``}
+  ${useAppleNetworking ? `s.dependency 'AFNetworking', '~> 3.0'` : `# ...`}
   # s.dependency "..."
 end
 
