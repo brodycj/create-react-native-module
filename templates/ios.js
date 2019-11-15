ea02b735daccec43925081bd238b30d68a51ffba
Chris Brody <chris@brody.consulting>
2019-11-15 02:02:03 -0500

    consistent iOS method spacing in generated code (#148)
    * continue using tabs in case of `useAppleNetworking: true`
    * with 4 spaces in case of no truthy `useAppleNetworking` option
    
    with a comment removed in case of `useAppleNetworking: true`

--- templates/ios.js
+++ templates/ios.js
@@ -48,18 +48,22 @@ ${useAppleNetworking ? `#import <AFNetworking/AFNetworking.h>
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-	${useAppleNetworking
-    ? `AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+` +
+    // sample method body with tabs for truthy useAppleNetworking *only*:
+    (useAppleNetworking
+      ? `{
+	AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
 	manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/plain"];
 	[manager GET:@"https://httpstat.us/200" parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
 			callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@ resp: %@", numberArgument, stringArgument, responseObject]]);
 	} failure:^(NSURLSessionTask *operation, NSError *error) {
 			callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@ err: %@", numberArgument, stringArgument, error]]);
-	}];`
-    : `callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);`}
-}
+	}];
+}`
+      : `{
+    // TODO: Implement some actually useful functionality
+    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+}`) + `
 
 @end
 `,
