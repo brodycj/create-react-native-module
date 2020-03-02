261e9b48d530ff5381cfbd0d7002235b47110eb4
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-03-02 12:04:50 -0500

    update Windows windows-csharp tests to test Windows (C#) template only

--- tests/unsupported-platforms/windows-csharp/with-defaults/windows-csharp-with-defaults.test.js
+++ tests/unsupported-platforms/windows-csharp/with-defaults/windows-csharp-with-defaults.test.js
@@ -1,32 +1,25 @@
-const lib = require('../../../../lib/lib.js');
+const windowsTemplate = require('../../../../templates/windows');
 
-// special compact mocks for this test
-// with mock uuid for Windows:
-jest.mock('uuid', () => ({
-  v1: () => 'E22606E0-B47F-11E9-A3F0-07F70A25DAFB'
-}));
-const mysnap = [];
-const mockpushit = x => mysnap.push(x);
-jest.mock('fs-extra', () => ({
-  outputFile: (outputFileName, theContent) => {
-    mockpushit({
-      outputFileName: outputFileName.replace(/\\/g, '/'),
-      theContent
-    });
-    return Promise.resolve();
-  },
-  ensureDir: (dir) => {
-    mockpushit({ ensureDir: dir.replace(/\\/g, '/') });
-    return Promise.resolve();
-  },
-}));
+// injected uuid:
+const uuid = 'E22606E0-B47F-11E9-A3F0-07F70A25DAFB';
 
 test('unsupported Windows C# template with defaults', () => {
   const options = {
-    platforms: ['windows'],
-    name: 'alice-bobbi',
+    // normalized for name: 'alice-bobbi':
+    name: 'AliceBobbi',
+    namespace: 'Alice.Bobbi',
+    // injected uuid:
+    uuid
   };
 
-  return lib(options)
-    .then(() => { expect(mysnap).toMatchSnapshot(); });
+  const mysnap = [];
+
+  windowsTemplate('windows').forEach(({ name, content }) => {
+    mysnap.push({
+      outputFileName: name(options),
+      theContent: content(options)
+    });
+  });
+
+  expect(mysnap).toMatchSnapshot();
 });
