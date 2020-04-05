66b557e20e5efe06b147cd0029fb818f9f0db84f
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-04-05 18:08:29 -0400

    add cli-action-error-undefined-args-missing-name.test.js
    reproduces issue 305 in lib/cli-command.js

---
+++ tests/with-injection/cli/command/action-error/cli-action-error-undefined-args-missing-name.test.js
@@ -0,0 +1,9 @@
+const action = require('../../../../../lib/cli-command.js').action;
+
+test('create lib module with undefined args - missing name', () => {
+  const args = undefined;
+
+  // reproduces the following error ref:
+  // https://github.com/brodybits/create-react-native-module/issues/305
+  expect(() => { action(args, {}); }).toThrow("Cannot read property '0' of undefined");
+});
