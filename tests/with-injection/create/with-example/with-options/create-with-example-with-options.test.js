42fd48f24fb2cb4f81e01ea37fe4ffca8dd38540
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-05-06 15:38:51 -0400

    quick test update with className library API option - resolves #336

--- tests/with-injection/create/with-example/with-options/create-with-example-with-options.test.js
+++ tests/with-injection/create/with-example/with-options/create-with-example-with-options.test.js
@@ -9,6 +9,7 @@ test('create alice-bobbi module with example, with config options including `exa
 
   const options = {
     name: 'alice-bobbi',
+    className: 'SuperAwesomeModule',
     tvosEnabled: true,
     githubAccount: 'alicebits',
     authorName: 'Alice',
