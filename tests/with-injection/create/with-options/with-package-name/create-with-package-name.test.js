9c64226c90b0347f0a50e3aad3b167ab6ad19921
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-03-15 23:56:24 -0400

    test: rename & update test with custom module package name

--- tests/with-injection/create/with-options/with-package-name/create-with-package-name.test.js
+++ tests/with-injection/create/with-options/with-package-name/create-with-package-name.test.js
@@ -2,14 +2,14 @@ const lib = require('../../../../../lib/lib.js');
 
 const ioInject = require('../../../helpers/io-inject.js');
 
-test(`create alice-bobbi module with moduleName: 'custom-native-module'`, async () => {
+test(`create alice-bobbi module package with custom packageName`, async () => {
   const mysnap = [];
 
   const inject = ioInject(mysnap);
 
   const options = {
     name: 'alice-bobbi',
-    moduleName: 'custom-native-module'
+    packageName: 'custom-native-module'
   };
 
   await lib(options, inject);
